b0149de954ad7c47cf94239399e9c10fc0b74df9
Vinicius Giroti <vinicius.giroti@gmail.com>
2025-06-20 00:21:19 -0300

    Bugou meu pc

--- Semantico.hs
+++ Semantico.hs
@@ -29,7 +29,24 @@ coercao op e1 e2 t1 t2 | t1 == t2                    = return (t1, op e1 e2)
                                                    " eh do tipo " ++ show t2 ++" \n"
                                         return (t1, op e1 e2)
 
+
+coercaoDiv op e1 e2@(Const(CInt 0)) t1 t2 = do errorMsg $ "Erro do divisor ser 0 na expressao: " ++ show (op e1 e2) ++ ", " ++ show (e2) ++ "eh 0" ++ " \n"
+                                               return (t1, op e1 e2)
+
 tExpr tfun tab (Const (CInt n)) = return (TInt, Const(CInt n))
 tExpr tfun tab (Const (CDouble n)) = return (TDouble, Const(CDouble n))
 tExpr tfun tab (Lit s) = return (TString, Lit s)
-tExpr tfun tab (Add e1 e2) = do {(t1, e1') <- tExpr tfun tab e1; (t2, e2') <- tExpr tfun tab e2; coercao Add e1' e2' t1 t2}
+tExpr tfun tab (Add e1 e2) = do {(t1, e1') <- tExpr tfun tab e1;
+                                 (t2, e2') <- tExpr tfun tab e2;
+                                 coercao Add e1' e2' t1 t2}
+tExpr tfun tab (Sub e1 e2) = do {(t1, e1') <- tExpr tfun tab e1;
+                                 (t2, e2') <- tExpr tfun tab e2;
+                                 coercao Sub e1' e2' t1 t2}
+tExpr tfun tab (Mul e1 e2) = do {(t1, e1') <- tExpr tfun tab e1;
+                                 (t2, e2') <- tExpr tfun tab e2;
+                                 coercao Mul e1' e2' t1 t2}
+
+tExpr tfun tab (Div e1 e2) = do {(t1, e1') <- tExpr tfun tab e1;
+                                 (t2, e2') <- tExpr tfun tab e2;
+                                 coercaoDiv Div e1' e2' t1 t2}
+          
